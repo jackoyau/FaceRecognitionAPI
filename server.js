a7343610c55814762202902d50b7f1189a612bdb
jackoyau <jackoyau555@gmail.com>
2021-07-07 17:32:58 +0800

    adding response

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req,res) => {res.send('it is working!')})
+
 app.post('/signin', (req,res)=>{ signin.handleSigin(req,res,db,bcrypt)}); //dependency injection
 
 app.post('/register', register.handleRegister(db, bcrypt)); // advance!!  pass function to function
